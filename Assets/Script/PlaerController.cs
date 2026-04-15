f297c2a2c4ee64a0de250aec29a8634f1f67b7af
haru452 <132886920+haru452@users.noreply.github.com>
2026-04-15 16:24:42 +0900

    a
    a

---
+++ Assets/Script/PlaerController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlaerController : MonoBehaviour
+{
+    private float speed = 25.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput =Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        //we'll move the vehicle forward
+        transform.Translate(Vector3.forward*Time.deltaTime * speed * forwardInput);
+       // we will turn the vehicle
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
